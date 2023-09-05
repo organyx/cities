82547154ef8fa0549faebecd4c0dd3ad8d00783b
AleksBeliun <aleksandr@nvoresume.com>
2023-09-05 13:50:27 +0200

    feat: :sparkles: Add middleware to validate incoming data

---
+++ src/middleware/validate.js
@@ -0,0 +1,25 @@
+const { ZodError } = require('zod');
+const { StatusCodes } = require('http-status-codes');
+
+function validate(schema) {
+  return (req, res, next) => {
+    try {
+      const input = {
+        params: req.params,
+        query: req.query,
+        body: req.body
+      };
+      schema.parse(input);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          status: 'fail',
+          errors: error.errors
+        });
+      }
+    }
+  };
+}
+
+module.exports = validate;
